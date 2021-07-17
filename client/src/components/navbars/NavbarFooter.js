b74e764db1c9246aa17125dc2de5ba2bb15ec2af
Blaine <itzablainething@gmail.com>
2021-07-17 15:57:00 -0400

    Landing Layout Responsive -V1

--- client/src/components/navbars/NavbarFooter.js
+++ client/src/components/navbars/NavbarFooter.js
@@ -9,15 +9,12 @@ export default function NavbarFooter() {
 	return (
 		<div>
             <Navbar expand="lg" fixed="bottom">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <Navbar.Text>Weather data provided by <Nav.Link href="https://openweathermap.org/api" className="navbarfooter-nav-item" target="_blank">OpenWeather API</Nav.Link></Navbar.Text>
                     </Nav>
                     <Nav className="ml-auto">
                         <Navbar.Text>Created by <a href="https://chandlerblainemorris.com/" id="navbarfooter-nav-portfolio" target="_blank">Chandler Morris</a></Navbar.Text>
                     </Nav>
-                </Navbar.Collapse>
             </Navbar>
 		</div>
 	)
